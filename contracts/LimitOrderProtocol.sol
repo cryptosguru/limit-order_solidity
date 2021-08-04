e6e9ff829f183c2d8cf2689b9181495be3b3a4bd
jackalstar88 <jackalstar88@gmail.com>
2021-08-04 13:41:53 +0300

    Use _callGetTakerAmount instead of ratio

--- contracts/LimitOrderProtocol.sol
+++ contracts/LimitOrderProtocol.sol
@@ -263,8 +263,8 @@ contract LimitOrderProtocol is
             else {
                 makingAmount = _callGetMakerAmount(order, takingAmount);
                 if (makingAmount > remainingMakerAmount) {
-                    takingAmount = (takingAmount * remainingMakerAmount) / makingAmount;
                     makingAmount = remainingMakerAmount;
+                    takingAmount = _callGetTakerAmount(order, makingAmount);
                 }
                 require(makingAmount >= thresholdAmount, "LOP: making amount too low");
             }
